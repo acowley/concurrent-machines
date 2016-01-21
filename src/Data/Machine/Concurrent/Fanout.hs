fe374a20d5f5d7fd8ff6f500c94ee683424b4623
Anthony Cowley <acowley@gmail.com>
2016-01-20 19:48:36 -0500

    Fix fanout issues (from Ben Sinclair)
    - Flush all waiting Yields before starting the fanout chain of awaits.
    - Stop when upstream stops or every member of the fanout has stopped.
    
    This code is very lightly adapted from that in the machines package.

--- src/Data/Machine/Concurrent/Fanout.hs
+++ src/Data/Machine/Concurrent/Fanout.hs
@@ -2,7 +2,7 @@
 -- | Provide a notion of fanout wherein a single input is passed to
 -- several consumers. The consumers are run concurrently.
 module Data.Machine.Concurrent.Fanout (fanout, fanoutSteps) where
-import Control.Arrow (second)
+import Control.Arrow (first, second)
 import Control.Concurrent.Async.Lifted (Async, async, wait)
 import Control.Monad (foldM)
 import Control.Monad.Trans.Control (MonadBaseControl, StM)
@@ -12,17 +12,18 @@ import Data.Maybe (catMaybes)
 import Data.Monoid (Monoid, mempty, mconcat)
 import Data.Semigroup (Semigroup(sconcat))
 import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.Coerce (coerce)
 
 -- | Feed a value to a 'ProcessT' at an 'Await' 'Step'. If the
 -- 'ProcessT' is awaiting a value, then its next step is
 -- returned. Otherwise, the original process is returned.
 feed :: forall m a b. MonadBaseControl IO m
      => a -> ProcessT m a b
-     -> m (Async (StM m ([b], Maybe (MachineStep m (Is a) b))))
+     -> m (Async (StM m (MachineStep m (Is a) b)))
 feed x m = async $ runMachineT m >>= \(v :: MachineStep m (Is a) b) ->
              case v of
-               Await f Refl _ -> runMachineT (f x) >>= flushYields
-               s -> return ([]::[b], Just s)
+               Await f Refl _ -> runMachineT (f x)
+               s -> return s
 
 -- | Like 'Data.List.mapAccumL' but with a monadic accumulating
 -- function.
@@ -37,11 +38,11 @@ mapAccumLM f z = fmap (second ($ [])) . foldM aux (z,id)
 -- 'MachineT'. Returns the list of yielded values and the next
 -- (non-Yield) step of the machine.
 flushYields :: Monad m
-            => Step k o (MachineT m k o) -> m ([o], Maybe (MachineStep m k o))
+            => Step k o (MachineT m k o) -> m ([o], Maybe (MachineT m k o))
 flushYields = go id
   where go rs (Yield o s) = runMachineT s >>= go ((o:) . rs)
         go rs Stop = return (rs [], Nothing)
-        go rs s = return (rs [], Just s)
+        go rs s = return (rs [], Just $ encased s)
 
 -- | Share inputs with each of a list of processes in lockstep. Any
 -- values yielded by the processes for a given input are combined into
@@ -50,12 +51,13 @@ fanout :: (Functor m, MonadBaseControl IO m, Semigroup r)
        => [ProcessT m a r] -> ProcessT m a r
 fanout xs = encased $ Await (MachineT . aux) Refl (fanout xs)
   where aux y = do (rs,xs') <- mapM (feed y) xs >>= mapAccumLM yields []
-                   let nxt = fanout . map encased $ catMaybes xs'
+                   let nxt = fanout $ catMaybes xs'
                    case rs of
                      [] -> runMachineT nxt
                      (r:rs') -> return $ Yield (sconcat $ r :| rs') nxt
-        yields rs (rs', Nothing) = return (rs' ++ rs, Nothing)
-        yields rs (rs', Just s) = return (rs' ++ rs, Just s)
+        yields rs Stop = return (rs,Nothing)
+        yields rs y@Yield{} = first (++ rs) <$> flushYields y
+        yields rs a@Await{} = return (rs, Just $ encased a)
 
 -- | Share inputs with each of a list of processes in lockstep. If
 -- none of the processes yields a value, the composite process will
@@ -68,9 +70,10 @@ fanoutSteps :: (Functor m, MonadBaseControl IO m, Monoid r)
             => [ProcessT m a r] -> ProcessT m a r
 fanoutSteps xs = encased $ Await (MachineT . aux) Refl (fanoutSteps xs)
   where aux y = do (rs,xs') <- mapM (feed y) xs >>= mapAccumLM yields []
-                   let nxt = fanoutSteps . map encased $ catMaybes xs'
+                   let nxt = fanoutSteps $ catMaybes xs'
                    if null rs
                    then return $ Yield mempty nxt
                    else return $ Yield (mconcat rs) nxt
-        yields rs (rs', Nothing) = return (rs' ++ rs,Nothing)
-        yields rs (rs', Just s) = return (rs'++rs, Just s)
+        yields rs Stop = return (rs,Nothing)
+        yields rs y@Yield{} = first (++rs) <$> flushYields y
+        yields rs a@Await{} = return (rs, Just $ encased a)
